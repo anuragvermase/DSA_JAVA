d733f5f3ec0a45fd7525385cd4dda7e30575fa3c
Anurag Verma <anuragver689@gmail.com>
2024-02-27 23:54:19 +0530

    output

---
+++ src/Indroduction/output.java
@@ -0,0 +1,9 @@
+package Indroduction;
+
+public class output {
+    public static void main(String[] args) {
+        System.out.println("Hello Anurag !");  //Hello Anurag !
+        System.out.println(12345);  //12345
+        System.out.println(10+9);  //19
+    }
+}
