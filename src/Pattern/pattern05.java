e15f9de5b63a01dd3430f651139095e555c5c59b
Anurag Verma <anuragver689@gmail.com>
2024-03-01 23:53:59 +0530

    pattern

--- src/Pattern/pattern05.java
+++ src/Pattern/pattern05.java
@@ -1,4 +1,39 @@
+/*
+*  Print :     *
+*             ***
+*            *****
+*             ***
+*              *
+* */
 package Pattern;
 
+import java.util.Scanner;
+
 public class pattern05 {
+
+    public static void main(String[] args) {
+        System.out.println("Give only Odd input number :");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int nst = 1;
+        int nsp = n/2;
+        for(int i=1; i<=n;i++){
+
+            for(int j=1;j<=nsp;j++){
+                System.out.print(" ");
+            }
+
+            for(int k=1;k<=nst;k++){
+                System.out.print("*");
+            }
+            if(i<=n/2) {
+                nsp--;
+                nst = nst + 2;
+            }else{
+                nsp++;
+                nst = nst - 2;
+            }
+            System.out.println();
+        }
+    }
 }
