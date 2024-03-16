527fb8ef754c6271182226cd3fffcd4b983528e4
Anurag Verma <anuragver689@gmail.com>
2024-03-16 23:14:07 +0530

    commit

---
+++ src/Array/reverseArray.java
@@ -0,0 +1,40 @@
+/*Reverse array : Given an array of length N, Reverse the whole array. */
+package Array;
+
+import java.util.Scanner;
+
+public class reverseArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        reverse(arr);
+
+        for (int i = 0; i < N; i++) {
+            System.out.println(arr[i]);
+
+        }
+    }
+
+    public static void reverse(int[] arr){
+        int sp = 0;                   //sp-starting point
+        int ep = arr.length-1;        //ep-ending point
+
+        while (sp<ep){             //condition
+            int temp = arr[sp];
+            arr[sp]=arr[ep];
+            arr[ep]=temp;
+
+            sp++;        //preparation for the next line
+            ep--;
+
+        }
+
+    }
+}
