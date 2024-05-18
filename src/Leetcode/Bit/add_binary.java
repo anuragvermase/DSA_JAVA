1ce0f1a3b1c4044c0427c42cc2d6d66c250cac6d
Anurag Verma <anuragver689@gmail.com>
2024-05-18 16:24:45 +0530

    add

---
+++ src/Leetcode/Bit/add_binary.java
@@ -0,0 +1,17 @@
+package Leetcode.Bit;
+
+import java.util.Scanner;
+
+public class add_binary {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+
+        addTwoBinary(str1,str2);
+
+    }
+    public static void addTwoBinary(String str1, String str2){
+
+    }
+}
