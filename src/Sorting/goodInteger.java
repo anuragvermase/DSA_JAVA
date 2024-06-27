e215709473cd737b024c577149ff98710af2f0af
Anurag Verma <anuragver689@gmail.com>
2024-06-27 21:47:35 +0530

    add

---
+++ src/Sorting/goodInteger.java
@@ -0,0 +1,34 @@
+/*
+* Good Integer :
+* Given arr[N], calculate no. of good integers. An element is said to be good if
+* {No. of element < ele == ele itself}
+*
+* eg: {-1 -4 3 5 -15 4} -> ele's
+*       2  1 3 5  0  4  -> no of element smaller than ele.
+* so 3 5 4 are good integer.
+* */
+package Sorting;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class goodInteger {
+    public static void main(String[] args) {
+        int[] arr = {-1, -4, 3, 5, -15, 4};
+
+         int r = noOfGoodInt(arr);
+        System.out.println(r);
+    }
+
+    public static int noOfGoodInt(int[] arr){
+        Arrays.sort(arr);
+        int count =0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]==i){
+                count =count +1;
+//                System.out.println(count);
+            }
+        }
+        return count;
+    }
+}
