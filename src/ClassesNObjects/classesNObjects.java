2e81c9ae8a873c7bc40d27bd440acb2dadb9fb74
Anurag Verma <anuragver689@gmail.com>
2024-07-12 14:19:14 +0530

    update

--- src/ClassesNObjects/classesNObjects.java
+++ src/ClassesNObjects/classesNObjects.java
@@ -7,25 +7,37 @@ public class classesNObjects {
 //        Great p2 = new Great();
 //        p2.y = 20;
 //        System.out.println(p2.y); //20
+
+        //After creating constructor.
+        //constructor with two parameter
         Great p3 = new Great(100,200);
         System.out.println(p3.x+p3.y); //300
         System.out.println(p3.y); //200
+        //constructor with no parameter
         Great p4 = new Great();
         System.out.println(p4.x); //0
         System.out.println(p4.y); //0
+
+//        Great p5 = new Great(10,20.0);
+//        System.out.println(p5.x); //0
+//        System.out.println(p5.y); //0
 
     }
     //creating class
     public static class Great{
         int x;
         int y;
-//        constructor
+       //constructor
         Great(int u, int v){
             x=u;
             y=v;
         }
         Great(){
 
+        }
+        Great(long d1, long d2){
+//            x=d1;
+//            y=d2;
         }
     }
 }
