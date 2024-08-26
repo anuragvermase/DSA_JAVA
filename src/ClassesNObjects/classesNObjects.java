1fec331af2f1b346ecc8afd2c172b445a00df2b5
anurag verma <anuragver689@gmail.com>
2024-08-26 19:02:21 +0530

    linkedlist

--- src/ClassesNObjects/classesNObjects.java
+++ src/ClassesNObjects/classesNObjects.java
@@ -2,25 +2,27 @@ package ClassesNObjects;
 
 public class classesNObjects {
     public static void main(String[] args) {
-//        Great p1 = new Great();
-//        System.out.println(p1.x); //0
-//        Great p2 = new Great();
-//        p2.y = 20;
-//        System.out.println(p2.y); //20
+         Great p1 = new Great();
+        // System.out.println(p1.x); //0
+         Great p2 = new Great();
+         p2.y = 20;
+        // System.out.println(p2.y); //20
 
-        //After creating constructor.
-        //constructor with two parameter
+        // After creating constructor.
+
+        // constructor with two parameter
         Great p3 = new Great(100,200);
         System.out.println(p3.x+p3.y); //300
         System.out.println(p3.y); //200
+
         //constructor with no parameter
         Great p4 = new Great();
         System.out.println(p4.x); //0
         System.out.println(p4.y); //0
 
-//        Great p5 = new Great(10,20.0);
-//        System.out.println(p5.x); //0
-//        System.out.println(p5.y); //0
+         Great p5 = new Great(10L, 20L);
+         System.out.println(p5.x); //100
+         System.out.println(p5.y); //200
 
     }
     //creating class
@@ -32,12 +34,14 @@ public class classesNObjects {
             x=u;
             y=v;
         }
+
         Great(){
 
         }
-        Great(long d1, long d2){
-//            x=d1;
-//            y=d2;
+
+         Great(long d1, long d2){
+               x = 100;
+               y = 200;
         }
     }
 }
